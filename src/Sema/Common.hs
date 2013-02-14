aa65a945ea88992de4af06a0a84f9b30fc9f0330
Lukas Kuklinek <ilja.kuklic@centrum.cz>
2013-02-14 19:48:52 +0100

    type variable symbol generation

--- src/Sema/Common.hs
+++ src/Sema/Common.hs
@@ -1,9 +1,11 @@
 
-module Sema.Common where
+module Sema.Common(Symbol(..), genSymbols) where
 
-newtype Symbol = Symbol String deriving Eq
+newtype Symbol = Symbol String deriving (Eq, Ord)
 
 instance Show Symbol where
     show (Symbol s) = s
 
+genSymbols = map Symbol . tail . concat $ iterate (\xss -> [x:xs | x <- symChars, xs <- xss]) [""]
+symChars  = ['a'..'d'] ++ ['g','h','m','n'] ++ ['p'..'v']
 
