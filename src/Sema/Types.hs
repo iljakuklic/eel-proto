17dac7f779fd7777f540c17ab71721ceb80fc5d1
Lukas Kuklinek <ilja.kuklic@centrum.cz>
2013-02-14 19:49:39 +0100

    type utility functions

--- src/Sema/Types.hs
+++ src/Sema/Types.hs
@@ -2,27 +2,33 @@
 
 module Sema.Types(
     -- * Data structures
-    Type(..), Type2(..),
-    -- * Helper functions
+    Type(..), Type2(..), Row,
+    -- * Type manipulation functions
+    toListUniq, tyRow, isTypeMono, isRowMono,
+    -- * Type representation smart constructors
     tUnit, tChar, tNum, tReal, tBool, tString, tMaybe
   ) where
 
 import Sema.Common
+
+import Prelude hiding (all)
 import Data.Foldable
 import Data.Monoid
-import Data.List
+import Data.List hiding (all)
 
 -- | Type specification representation.
 data Type v = TAtom Symbol                  -- ^ atomic type
             | TVar  v                       -- ^ type variable
-            | TBin Type2 (Type v) (Type v)  -- ^ binary type
             | TList (Type v)                -- ^ list type
+            | TBin Type2 (Type v) (Type v)  -- ^ binary type
             deriving Eq
 
 -- | Compound type variants (sum, product, function)
 data Type2 = TSum | TProd | TFunc deriving Eq
 
--- functor
+-- | Row is a list of types
+newtype Row v = Row [Type v] deriving (Eq)
+
 instance Functor Type where
     fmap f (TVar v) = TVar (f v)
     fmap f (TList t) = TList (fmap f t)
@@ -35,8 +41,28 @@ instance Foldable Type where
     foldMap f (TBin _ a b) = foldMap f a <> foldMap f b
     foldMap f (TAtom _) = mempty
 
--- | collect type variables
-tyVars = nub . toList
+-- forward row instances to list
+instance Functor  Row where fmap    f (Row xs) = Row $ (fmap . fmap) f xs
+instance Foldable Row where foldMap f (Row xs) = foldMap (foldMap f) xs
+
+-- | Collect list of type variables
+toListUniq :: (Eq a, Foldable t) => t a -> [a]
+toListUniq = nub . toList
+
+-- | Extract row spine from type (top of the stack in in the beginning)
+tyRow :: (Eq v) => Type v -> Row v
+tyRow = Row . tyRow'
+  where
+    tyRow' (TBin TProd t1 t2) = t2 : tyRow' t1
+    tyRow' t = [t]
+
+-- | Chack if type is monomorphic (no type variables)
+isTypeMono :: Type v -> Bool
+isTypeMono = null . toList
+
+-- | Check if row is monomorphic (no type vars except row polymorphism)
+isRowMono :: Row v -> Bool
+isRowMono (Row xs) = all isTypeMono (init xs)
 
 -- | Create atom type
 tAtom name = TAtom (Symbol name)
@@ -90,10 +116,12 @@ instance (Eq v, Show v) => Show (Type v) where
       where
         parF = case (p, c) of (False, TProd) -> id; _ -> parens
         p' TFunc = (False, False)
-        p' TProd = (p, True)
+        p' TProd = (False, True)
         p' TSum  = (True, True)
         (pl, pr) = p' c
         parens str = "(" ++ str ++ ")"
         t_op c = case c of TSum -> " | "; TProd -> ", "; TFunc -> " -> "
 
+instance (Eq v, Show v) => Show (Row v) where
+  show (Row xs) = "[[" ++ show (reverse xs) ++ ">>"
 
