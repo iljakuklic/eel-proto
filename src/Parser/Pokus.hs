0368673ce231c56800f0d769d1921a85e654ec51
Lukas Kuklinek <ilja.kuklic@centrum.cz>
2013-03-30 02:14:17 +0100

    experimental parser update

--- src/Parser/Pokus.hs
+++ src/Parser/Pokus.hs
@@ -1,4 +1,6 @@
 
+module Parser.Pokus where
+
 import Builtins.Eval
 import Sema.Term
 import Parser.State
@@ -17,14 +19,27 @@ initState = PState { pSymTable = initSymTab, pRules = M.empty, pStack = Stack []
 
 ptok p = p <* spaces
 pstok = ptok . string
+psbet a b = between (pstok a) (pstok b)
 pterm =  TComp () <$> pfunc <*> pterm
      <|> pure (TFunc () (FCBuiltIn BIid))
-pfunc = TFunc () <$> (ptok psymb >>= lookupFuncCall)
-     <|> TQuot () <$ pstok "[" *> pterm <* pstok "]"
+pfunc =  TFunc () <$> (ptok psymb >>= lookupFuncCall)
+     <|> TQuot () <$> psbet "[" "]" pterm
      <|> TInt () . read <$> ptok (many1 digit)
+     <|> psbet "'" "'" pstr
+pstr  = TList () <$> many pchr
+pchr  = TChar () <$> satisfy (\ch -> isPrint ch && ch /= '\'')  -- TODO escape seqs
 psymb = Symbol <$> ((:) <$> satisfy isAlpha <*> many alphaNum)
 peval = () <$ many (pfunc >>= eval)
 ptop  = spaces >> peval >> eof >> getState
 
-doparse = runParser ptop initState "<here>"
+doparse str =
+    case runParser ptop initState "<here>" str of
+        Left err  -> putStrLn (show err)
+        Right ste -> printState ste
+
+printState (PState st _rul (Stack stk)) = do
+    putStrLn "Funcs:"
+    mapM_ putStrLn [ show n ++ " = " ++ show d | (n, FDUser d) <- M.toList st]
+    putStrLn "Stack:"
+    mapM_ (putStrLn . show) stk
 
