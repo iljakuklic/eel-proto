55e6b631dbd2f5dcf0b25a3909560ca38f57b71a
Lukas Kuklinek <ilja.kuklic@centrum.cz>
2013-05-15 13:45:17 +0200

    updated backend

--- src/Backend/CodeGen.hs
+++ src/Backend/CodeGen.hs
@@ -4,7 +4,7 @@ module Backend.CodeGen(
         -- * Code generation functions
         fill, codegen,
         -- * LLVM types
-        LLType, llTypeDoc, llInt, llRaw, llPair, llSum, llFloat, llChar, llStack,
+        LLType, llTypeDoc, llInt, llIntNative, llRaw, llPair, llSum, llFloat, llChar, llNamed, llStruct, llPtr, llSize, llBool,
         -- * Platform specifics
         ptrBytes, ptrBits, sizeBits, intBits, charBits,
         -- * Variables and identifiers
@@ -12,13 +12,13 @@ module Backend.CodeGen(
         llVarInt, llVarChar, llVarFloat, llVarSize, llVarNull,
         freshRaw, fresh, fresh1, freshGlobal1,
         -- * Stack manipulation
-        getStack, setStack, push', pop', dipn,
+        getStack, setStack,
         -- * Genral code emission
-        mapCode, appendRaw, append, appendTmpl,
-        -- * LLVM rendering
-        label, instr, comment, braceBlock,
-        -- * Common LLVM instructions
-        bitcast, callRaw
+        mapCode, appendRaw, append, appendTmpl, blankLine,
+        -- * LLVM constructs
+        label, instr, comment, braceBlock, funcHeader, funcPrototype,
+        -- * LLVM instructions
+        phi, branch, branchCond, gep, gepFst, gepSnd, load, store, bitcast, callRaw, ret
     ) where
 
 import Control.Monad.State(get, put, modify, runState)
@@ -69,14 +69,20 @@ llTypesDoc = P.hsep . P.punctuate P.comma . fmap llTypeDoc
 -- helpers
 llInt  = LLInt ptrBits
 llBool = LLInt 1
-llChar = LLInt intBits
+llChar = LLInt charBits
 llRaw  = LLPtr (LLInt 8)
 llPairT a b = LLStruct [a, b]
 llPair = llPairT llRaw llRaw
 llSum  = llPairT llRaw llBool
 llFloat = if ptrBits == 64 then LLDouble else LLFloat
 llSize = LLInt sizeBits
-llStack = LLNamed "%eel.stack" 
+llNamed = LLNamed
+llStruct = LLStruct
+llPtr = LLPtr
+llIntNative = LLInt intBits
+llIsFloating LLFloat = True
+llIsFloating LLDouble = True
+llIsFloating _ = False
 
 -- | LLVM variable
 data LLVar = LLVar LLType String deriving (Eq)
@@ -86,7 +92,8 @@ instance Show LLVar where show = show . llVarDoc
 
 -- | convert LLVM variable to a doc
 llVarDoc (LLVar ty name) = llTypeDoc ty <+> P.text name
-
+-- | Doc-ify a number of comma-separated variables
+llVarsDoc = P.sep . P.punctuate P.comma . map llVarDoc
 -- | get LLVM variable name
 llVarName (LLVar _ty nm) = nm
 
@@ -103,7 +110,7 @@ llVarNull  = LLVar llRaw "null"
 
 -- | Code generator state
 data CodeGenState = CodeGenState {
-        cgStack  :: [ LLVar ],         -- ^ Stack of LLVM variables
+        cgStack  :: LLVar,             -- ^ pointer to the top of the stack
         cgVars   :: M.Map String Int,  -- ^ fresh variables counter
         cgOutput :: P.Doc              -- ^ generated document with source
     } deriving Show
@@ -131,20 +138,6 @@ getStack = cgStack <$> get
 -- | set current stack
 setStack stk = modify (\ste -> ste { cgStack = stk })
 
--- | Push an item onto the stack
-push' x = modify (\ste -> ste { cgStack = x : cgStack ste })
-
--- | Pop an item from the stack
-pop' = do (x:xs) <- getStack; setStack xs; return x
-
--- | perform code generation n levels deep in the stack
-dipn n cgen = do
-    (topn, stk) <- splitAt n <$> getStack
-    setStack stk
-    x <- cgen
-    modify (\ste -> ste { cgStack = topn ++ cgStack ste })
-    return x
-
 -- | Modify the code generated so far
 mapCode f = modify (\ste -> ste { cgOutput = f $ cgOutput ste})
 
@@ -160,6 +153,9 @@ append = appendRaw . indent
 -- | Add code by filling a templete
 appendTmpl tmpl = append . fill tmpl
 
+-- | Add a blank line
+blankLine = appendRaw (P.text "")
+
 -- | Fill arguments into the string template
 fill tmpl args = P.text $ fill' tmpl args
   where
@@ -170,18 +166,18 @@ fill tmpl args = P.text $ fill' tmpl args
     fill' [] (_:_) = error ("Excessive arguments in " ++ tmpl)
 
 -- | indent the source text
-indent = P.nest 4
+indent = P.nest 8
 
 -- | Convert any Show-able type to a Doc
 showDoc = P.text . show
 
 -- | Emit label LLVM code
-label var = append (showDoc var <> P.colon)
+label lab = appendRaw (P.nest 4 (P.text lab <> P.colon))
 
 -- | Emit an instruction w/ return value
 instr ty varname tmpl args = do
     var <- fresh1 ty varname
-    append (llVarDoc var <+> P.equals <+> fill tmpl args)
+    append (P.text (llVarName var) <+> P.equals <+> fill tmpl args)
     return var
 
 -- | Emit a comment
@@ -190,8 +186,39 @@ comment text args = append (P.semi <+> fill text args)
 -- | Enclose a document in braces
 braceBlock doc = P.lbrace $+$ indent doc $+$ P.rbrace
 
+-- | type conversion instrunction
+bitcast ty nme val@(LLVar vty _) | vty == ty = return val
+bitcast ty@(LLInt _) nme val@(LLVar (LLPtr _) _) = instr ty nme "ptrtoint # to #" [llVarDoc val, llTypeDoc ty]
+bitcast ty@(LLPtr _) nme val@(LLVar (LLInt _) _) = instr ty nme "inttoptr # to #" [llVarDoc val, llTypeDoc ty]
+bitcast ty' nme val@(LLVar (LLPtr _) _)    | llIsFloating ty' = bitcast llInt "tmp" val >>= bitcast ty' nme
+bitcast ty'@(LLPtr _) nme val@(LLVar ty _) | llIsFloating ty  = bitcast llInt "tmp" val >>= bitcast ty' nme
 bitcast ty nme val = instr ty nme "bitcast # to #" [llVarDoc val, llTypeDoc ty]
-callRaw ty name fname args = instr ty name "call # #(#)" [P.text fname, llTypeDoc ty, P.hsep . P.punctuate P.comma . fmap llVarDoc $ args]
+-- | getelementptr instruction
+gep ty nme var idxs = instr ty nme "getelementptr #, #" [llVarDoc var, llVarsDoc $ map (LLVar (LLInt 32) . show) idxs]
+-- | get a pointer to the first member of a pair
+gepFst ty nme var = gep ty nme var [0, 0 :: Int]
+-- | get a pointer to the second member of a pair
+gepSnd ty nme var = gep ty nme var [0, 1 :: Int]
+-- | load a value from the memory
+load ty nme ptr = instr ty nme "load #" [ptr]
+-- | store a value to the memory
+store val ptr = appendTmpl "store #, #" [val, ptr]
+-- | function call
+callRaw ty name fname args = instr ty name "call # #(#)" [llTypeDoc ty, P.text fname, llVarsDoc args]
+-- | return
+ret val = appendTmpl "ret #" [val]
+-- | LLVM SSA phi node
+phi ty name entries = instr ty name "phi # #" [llTypeDoc ty, inbbs]
+  where inbbs = P.sep $ P.punctuate P.comma [ P.brackets $ P.text v <> P.comma <+> P.char '%' <> P.text l | (LLVar _ v, l) <- entries]
+-- | branch
+branch lab = appendTmpl "br label %#" [P.text lab]
+-- | conditional branch
+branchCond var thenL elseL = appendTmpl "br #, label %#, label %#" [llVarDoc var, P.text thenL, P.text elseL]
+
+-- | function header
+funcHeader rtype fname input = P.text "define" <+> llTypeDoc rtype <+> P.text fname <> P.parens (llVarsDoc input)
+-- | function prototype
+funcPrototype rtype fname input = appendRaw $ P.text "declare" <+> llTypeDoc rtype <+> P.text fname <> P.parens (llTypesDoc input)
 
 -- | Run code generator
-codegen g = fst $ runState (g >> getCode) (CodeGenState [] M.empty P.empty)
+codegen g = fst $ runState (g >> getCode) (CodeGenState llVarNull M.empty P.empty)
--- src/Backend/Emit.hs
+++ src/Backend/Emit.hs
@@ -1,33 +1,51 @@
 
 
-module Backend.Emit(emitTerm, codegen, genTerm) where
+module Backend.Emit(emitTerm, emitModule, codegen, genTerm) where
 
 import Sema.Term
 import Sema.Types
 import Backend.CodeGen
-import Parser.State
+import Backend.Preamble
 
 --import Control.Applicative
 import qualified Text.PrettyPrint as P
-import Text.PrettyPrint(($+$), (<+>), (<>))
+import Text.PrettyPrint((<+>))
+import qualified Data.Map as M
 
-genTerm = show . codegen . emitTerm
+-- | Emit LLVM code for all entries in a symbol table
+emitModule mainName symTab = codegen $ do
+    preamble (fmap eelName mainName)
+    sequence [ blankLine >> emitFunc (show n) t | (n, FDUser t) <- M.toList symTab ]
 
-takeRows term = (tyRow tyIn, tyRow tyOut)
-  where Right (TyBin (TyFunc _) tyIn tyOut) = termType term
+-- | 
+genTerm = show . codegen . emitFunc "cmdline"
 
--- | Emit EEL code for a function
-emitFunc fname term = do
-    appendRaw (P.text "; Function definition:" <+> P.text fname <+> (P.parens . P.text . show . termType $ term))
+-- | Emit EEL code for a function from a term
+emitFunc fname term = generateFunc (eelName fname) (emitTerm term)
+
+-- | Emit EEL code for a function using a generator
+generateFunc fname gen = do
+    blankLine
+    input <- fresh1 llStack "stk"
+    setStack input
+    appendRaw (funcHeader llStack fname [input] <+> P.lbrace)
+    () <- gen
+    blankLine
+    comment "Return value" ([] :: String)
+    output <- getStack
+    ret output
+    appendRaw P.rbrace
     
-
-    
+eelName nme = "@eel." ++ nme
 
 -- | Emit code for EEL term
 emitTerm (TComp _ f g)   = emitTerm f >> emitTerm g
 emitTerm (TFunc _ nme t) = emitFD nme t
 emitTerm (TQuot _ f)     = comment "Quoting not implemented: #" [f]
-emitTerm t = emitVal t
+emitTerm t = do
+    blankLine
+    comment "Pushing value # to the stack" [t]
+    emitVal t
 
 -- | Emit code for EEL value
 emitVal (TInt _ x)   = push $ llVarInt   x
@@ -35,38 +53,41 @@ emitVal (TFloat _ x) = push $ llVarFloat x
 emitVal (TChar _ x)  = push $ llVarChar  x
 emitVal v = comment "Not implemented: #" [v]
 
-emitFD nme (FDUser _term) = comment "Not implemented: function call (#)" [nme]
-emitFD _nme (FDBuiltIn bi) = emitBI bi
+-- | Emit function call or builtin code
+emitFD nme (FDUser _term) = do
+    blankLine
+    comment "Call #" [nme]
+    inp <- getStack
+    out <- callRaw llStack "stk" (eelName $ show nme) [inp]
+    setStack out
+emitFD _nme (FDBuiltIn bi) = do
+    blankLine
+    comment "Emitting builtin #" [drop 2 (show bi)]
+    emitBI bi
 
 -- combinators
-emitBI BIzap = pop >> return ()
-emitBI BIdup = do x <- pop; push x; push x
+emitBI BIzap = pop llRaw "dead" >> return ()
+emitBI BIdup = do x <- pop llRaw "dup"; push x; push x
 emitBI BIid  = return ()
 emitBI BIid2  = return ()
 -- integers
 emitBI BIadd = arith llInt "add"
 emitBI BIsub = arith llInt "sub"
 emitBI BImul = arith llInt "mul"
-emitBI BIdiv = arith llInt "div"
+emitBI BIdiv = arith llInt "sdiv"
+-- floats
 emitBI BIfadd = arith llFloat "fadd"
 emitBI BIfsub = arith llFloat "fsub"
 emitBI BIfmul = arith llFloat "fmul"
 emitBI BIfdiv = arith llFloat "fdiv"
+-- catch
 emitBI bi = comment "Builtin not implemented: #" [drop 2 $ show bi]
 
 arith ty iname = do
-    y <- pop
-    x <- pop
-    y' <- bitcast ty "y" y
-    x' <- bitcast ty "x" x
-    r <- instr ty "n" (iname ++ " #, " ++ llVarName y') [x']
-    r' <- bitcast llRaw "r" r
-    push r'
-
--- | Allocate a piece of memory for given type
-alloc ty nme = do
-    ptr <- callRaw llRaw "ptr" "call" [llVarSize (llSizeOf ty)]
-    bitcast (llType ty) nme ptr
+    y <- pop ty "y"
+    x <- pop ty "x"
+    r <- instr ty "r" (iname ++ " #, " ++ llVarName y) [x]
+    push r
 
 -- | Get size of EEL type in bytes for allocation
 llSizeOf (TyBin TyProd _ _) = 2 * ptrBytes
@@ -86,3 +107,26 @@ llType (TyBin (TyFunc _) _ _) = llPair
 llType (TyVar _) = llRaw
 llType (TyPhase _) = error "No LLVM counterpart to a phase type"
 
+-- | Push an item onto the stack
+push x = do
+    oldStk <- getStack
+    newStk' <- alloc llStack "stk" (2 * ptrBytes)
+    setStack newStk'
+    newStk <- bitcast llStackPtr "tstk" newStk'
+    nextStk <- gepFst (llPtr llStack) "nstk" newStk
+    store oldStk nextStk
+    valStk <- gepSnd (llPtr llRaw) "vstk" newStk
+    x' <- bitcast llRaw "push" x
+    store x' valStk
+
+-- | Pop an item from the stack
+pop ty nme = do
+    oldStk' <- getStack
+    oldStk <- bitcast (llStackPtr) "tstk" oldStk'
+    valPtr <- gepSnd (llPtr llRaw) "vptr" oldStk
+    valPtr' <- bitcast (llPtr ty) "val" valPtr
+    val <- load ty nme valPtr'
+    newStkP <- gepFst (llPtr llStack) "pstk" oldStk
+    newStk <- load llStack "stk" newStkP
+    setStack newStk
+    return val
